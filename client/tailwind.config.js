f2ffa1a7177461bc7da8e21c9a7d321d54297aa4
gupta-shashwat <stgupta9935@gmail.com>
2023-09-18 00:26:05 +0530

    Configured the color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      background: {
+        DEFAULT: '#f4f5fa',
+      },
+      primary: {
+        DEFAULT: '#9155fd',
+        light: '#c5a6fe'
+      },
+      font: {
+        DEFAULT: '#534f5a',
+        light: '#79767e',
+      },
+      accent: {
+        green: '#56ca00',
+        red: '#ff3e1d',
+        wait: '#ffb400',
+        cyan: '#16b1ff',
+        gray: '#8a8d93',
+      }
+    }
   },
   plugins: [],
 }
