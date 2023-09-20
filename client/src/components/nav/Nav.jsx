049dc51f6c0ee63ac2d76be78621b19eaf5f3c3c
Ashutosh <vashutosh625@gmail.com>
2023-09-21 01:58:04 +0530

    bug fix

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
 							className="text-[#8b7d77] border-0 absolute w-full h-full text-lg px-5 py-2.5 rounded-[3px] left-0 top-0"
 						/>
 						<div className="bg-slate-700 absolute w-[60px] h-full z-[1] cursor-pointer right-0 top-0">
-							<i className="fas fa-search absolute  -translate-x-2/4 -translate-y-2/4 text-white text-[20px] left-2/4 top-2/4"></i>
+							<FontAwesomeIcon icon="fas fa-search" className="absolute  -translate-x-2/4 -translate-y-2/4 text-white text-[20px] left-2/4 top-2/4"/>
 						</div>
 					</div>
 				</div>
