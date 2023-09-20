4d6fbf2b81f46eba4a25c3842a53e8ab59479b8e
gupta-shashwat <stgupta9935@gmail.com>
2023-09-21 03:06:28 +0530

    refactored client side

--- client/src/components/SignupSPOC.jsx
+++ client/src/components/SignupSPOC.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const SignupSPOC = () => {
   return (
-  <>
-   <div className="mb-3">
-  <label for="text" className="form-label">University</label>
-  <input className="form-control" type="text" id="text"/>
-</div>
-<div className="mb-3">
-  <label for="text" className="form-label">Enrollment Number</label>
-  <input className="form-control" type="text" id="text"/>
-</div>
-<div className="mb-3">
-  <label for="email" className="form-label">Work Email</label>
-  <input className="form-control" type="email" id="email" required/>
-</div>
+    <>
+      <div className="mb-3">
+        <label for="text" className="form-label">University</label>
+        <input className="form-control" type="text" id="text" />
+      </div>
+      <div className="mb-3">
+        <label for="text" className="form-label">Enrollment Number</label>
+        <input className="form-control" type="text" id="text" />
+      </div>
+      <div className="mb-3">
+        <label for="email" className="form-label">Work Email</label>
+        <input className="form-control" type="email" id="email" required />
+      </div>
 
-<div class="mb-3">
-  <label for="formFileMultiple" class="form-label">ID Card</label>
-  <input class="form-control" type="file" id="formFileMultiple" multiple/>
-</div>
+      <div class="mb-3">
+        <label for="formFileMultiple" class="form-label">ID Card</label>
+        <input class="form-control" type="file" id="formFileMultiple" multiple />
+      </div>
 
-</>
-       
-    
+    </>
+
+
   )
 }
 
