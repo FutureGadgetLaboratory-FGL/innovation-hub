334fa3ba2dca05f2c4f950f7b8b61820208d9fa9
gupta-shashwat <stgupta9935@gmail.com>
2023-09-21 15:39:02 +0530

    Added pincode to university schema

--- server/models/University.js
+++ server/models/University.js
@@ -6,6 +6,7 @@ const universitySchema = mongoose.Schema({
     city: { type: String, required: true },
     state: { type: String, required: true },
     country: { type: String, required: true },
+    pincode: { type: Number, required: true },
     email: {
         type: String,
         required: true,
